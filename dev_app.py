bfecd0ba6ac40603ee1a43fc11df88068c17e69b
Vikas singh <93881110+vikasIITM@users.noreply.github.com>
2023-01-25 14:46:07 +0530

    Update dev_app.py
    removed bug

--- dev_app.py
+++ dev_app.py
@@ -224,7 +224,7 @@ if option == 'CRU 25KM Final Rain':
     1901, 2021,(2000,2005 ),step = 1)
     
 
-    file_name =  "CRU_precipitation_25k.csv"
+    file_name =  "CRU_precipitation_25k (1).csv"
     #st.write('Full path is ',base_path + file_name)
     df = pd.read_csv(file_name)
     ndf = action_new(df,period[0],period[1])
@@ -246,7 +246,7 @@ if option == 'CRU 25KM Final Temp':
     1901, 2021,(2000,2005 ),step = 1)
 
 
-    file_name =  "CRU_temp_25k.csv"
+    file_name =  "CRU_temp_25k (1).csv"
     #st.write('Full path is ',base_path + file_name)
     df = pd.read_csv(file_name)
     ndf = action_new(df,period[0],period[1])
